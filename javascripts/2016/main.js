4a6f4feca5a90b4f3117a3807d44c754ddeb199e
kmdr@wercker.com <kmdr@wercker.com>
2017-05-05 10:12:58 +0000

    Automated commit at 2017-05-05 10:12:57 UTC by middleman-deploy 1.0.0

---
+++ javascripts/2016/main.js
@@ -0,0 +1,1 @@
+(function(){$(document).ready(function(){var n;return n=["title","landing","band1","band2","band3","band4","band5","band6","band7","band8","band9","about"],$("#container").fullpage({anchors:n,menu:"#menu",continuousVertical:!0,scrollingSpeed:700}),this})}).call(this);
